e238a862953fb1016c25fb279456e144ee6a561e
YangWenjun <18244688848@163.com>
2018-05-27 15:36:53 +0800

    Release 1.0.0.

---
+++ YJMarqueeViewDemo/YJMarqueeViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YJMarqueeViewDemo
+//
+//  Created by YangWenjun on 2018/5/27.
+//  Copyright © 2018年 YangWenjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
